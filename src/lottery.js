d73ee770e6e1088d3d974833cad918783ccf529c
Yoshik18 <yoshikkarnawat@gmail.com>
2022-06-08 13:12:56 +0530

    Upload on sit

--- src/lottery.js
+++ src/lottery.js
@@ -1,7 +1,7 @@
 import web3 from './web3';
 
-const address = '';
+const address = '0xec713b05900Ed80aD45ceDD7964749292cff8a35';
 
-const abi = "";
+const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
 
 export default new web3.eth.Contract(abi, address);
